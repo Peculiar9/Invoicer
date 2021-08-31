75645211cba4e22bf8aefde87ccd7add956b0cc8
Peculiar Babalola <omobabson@gmail.com>
2021-08-31 18:13:18 +0100

    Deleted the project and replaced with a new one

---
+++ Invoicier/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Invoicier.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
